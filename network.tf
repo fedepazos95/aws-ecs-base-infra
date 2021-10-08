a2df3c5a1404e164f09f86c8c16d71033230995e
Federico Pazos <federico.pazos@vibia.com>
2021-10-08 19:08:10 +0200

    simplifies network.tf

--- network.tf
+++ network.tf
@@ -1,12 +1,18 @@
 ### Network
 
-# Fetch AZs in the current region
-data "aws_availability_zones" "available" {}
-
-resource "aws_vpc" "main" {
-  cidr_block = var.environment_cidr[terraform.workspace]
+resource "aws_ecs_cluster" "cluster" {
+  name = "cluster-example-${terraform.workspace}"
 
   tags = {
-    Name = "vpc-${var.vpc_name[terraform.workspace]}"
+    Environment = terraform.workspace
+  }
+}
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "vpc-example-${terraform.workspace}"
+    Environment = terraform.workspace
   }
 }
